e8df4e43e7418d3ecf289408f38819376ffee6ca
Pavani260410 <69670894+Pavani260410@users.noreply.github.com>
2022-02-01 19:00:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,3 +12,26 @@ function take_snapshot() {
         document.getElementById("results").innerHTML = '<img id="selfie_image" src="' + data_uri + '"/>';
     });
 }
+console.log("ml5 version: ", ml5.version);
+var classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/8fmEzf7zZ/model.json', modelLoaded);
+
+
+function modelLoaded() {
+    console.log("Model Loaded");
+}
+
+function check() {
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+}
+
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(2) * 100 + "%";
+    }
+}
